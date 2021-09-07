5571310fc4d5d5fec086660c911b1a6986ea27f2
Jonas Imping <j.imping@splintnet.de>
2021-09-08 00:24:29 +0200

    impr.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -177,7 +177,7 @@ defaults write com.apple.finder FXInfoPanesExpanded -dict \
 #defaults write com.apple.screencapture "location" -string "~/Documents/Screenshots"
 
 # Exclude date and time in screenshot filenames
-defaults write com.apple.screencapture "include-date" -bool false
+# defaults write com.apple.screencapture "include-date" -bool false
 
 # Change the default screenshot file name
 defaults write com.apple.screencapture "name" -string "screenshot"
