af0322666e859e76da8930252a45ebb4feed03a6
Jonas Imping <j.imping@splintnet.de>
2021-12-03 09:20:48 +0100

    remove unnecessary stuff

--- macos/apps.sh
+++ macos/apps.sh
@@ -6,7 +6,7 @@ mas install 866773894 # Quiver
 mas install 1333542190 # 1 Password
 mas install 1518425043 # Boop
 mas install 1287239339 # ColorSlurp
-mas install 1468691718 # Jayson
+#mas install 1468691718 # Jayson
 mas install 405772121 # LittleIpsum
 mas install 441258766 # Magnet
 mas install 1063631769 # Medis
@@ -14,7 +14,7 @@ mas install 1153157709 # SpeedTest
 mas install 747648890 # Telegram
 mas install 425424353 # Unarchiver
 mas install 1147396723 # Whatsapp
-mas install 595191960 # CopyClip
+# mas install 595191960 # CopyClip
 mas install 1518425043 # Boop
 mas install 409201541 # Pages
 mas install 409183694 # Keynote
