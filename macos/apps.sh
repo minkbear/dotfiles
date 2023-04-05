b7144eb47ce71c5fe9cf1ad60247a32f60081a03
Jonas Imping <j.imping@splintnet.de>
2023-04-05 13:51:53 +0200

    impr.

--- macos/apps.sh
+++ macos/apps.sh
@@ -3,7 +3,6 @@ mas install 1518036000 # Sequel Ace
 mas install 1436522307 # Transmit
 mas install 937984704 # Amphetamine
 mas install 866773894 # Quiver
-mas install 1333542190 # 1 Password
 mas install 1518425043 # Boop
 mas install 1287239339 # ColorSlurp
 #mas install 1468691718 # Jayson
@@ -15,7 +14,6 @@ mas install 747648890 # Telegram
 mas install 425424353 # Unarchiver
 mas install 1147396723 # Whatsapp
 # mas install 595191960 # CopyClip
-mas install 1518425043 # Boop
 mas install 409201541 # Pages
 mas install 409183694 # Keynote
 mas install 409203825 # Numbers
@@ -23,7 +21,6 @@ mas install 806101067 # QVR Client
 mas install 497799835 # xCode
 mas install 1274495053 # Microsoft To Do
 
-brew install --cask skype
 brew install --cask tower
 brew install --cask babeledit
 brew install --cask visual-studio-code
@@ -34,17 +31,13 @@ brew install --cask iterm2
 brew install --cask postman
 brew install --cask anydesk
 brew install --cask tunnelblick
-brew install --cask jetbrains-toolbox
-brew install --cask phpstorm
-brew install --cask datagrip
-brew install --cask termhere
+# brew install --cask termhere
 brew install --cask little-snitch
 brew install --cask imageoptim
-brew install --cask virtualbox
+# brew install --cask virtualbox
 brew install --cask adobe-creative-cloud
 brew install --cask zoom
-brew install --cask mongodb-compass
-brew install --cask signal
+# brew install --cask mongodb-compass
+# brew install --cask signal
 brew install --cask parallels
-brew install --cask microsoft-teams
 brew install --cask omnidisksweeper
--- shell/.zshrc
+++ shell/.zshrc
@@ -99,3 +99,9 @@ eval $(thefuck --alias)
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Ruby
+if [ -d "/opt/homebrew/opt/ruby/bin" ]; then
+  export PATH=/opt/homebrew/opt/ruby/bin:$PATH
+  export PATH=`gem environment gemdir`/bin:$PATH
+fi
