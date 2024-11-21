e4767e8c1c77c81623165d6425faa91ede4d8feb
Minkbear <minkbear@gmail.com>
2024-11-21 21:53:29 +0700

    Update apps.sh

--- macos/apps.sh
+++ macos/apps.sh
@@ -1,46 +1,46 @@
-mas install 803453959  # Slack
-mas install 1518036000 # Sequel Ace
-mas install 1436522307 # Transmit
-mas install 937984704  # Amphetamine
-mas install 1518425043 # Boop
-mas install 1287239339 # ColorSlurp
+#mas install 803453959  # Slack
+#mas install 1518036000 # Sequel Ace
+#mas install 1436522307 # Transmit
+#mas install 937984704  # Amphetamine
+#mas install 1518425043 # Boop
+#mas install 1287239339 # ColorSlurp
 #mas install 1468691718 # Jayson
-mas install 405772121  # LittleIpsum
-mas install 441258766  # Magnet
-mas install 1063631769 # Medis
-mas install 1153157709 # SpeedTest
-mas install 747648890  # Telegram
+#mas install 405772121  # LittleIpsum
+#mas install 441258766  # Magnet
+#mas install 1063631769 # Medis
+#mas install 1153157709 # SpeedTest
+#mas install 747648890  # Telegram
 mas install 425424353  # Unarchiver
-mas install 1147396723 # Whatsapp
+#mas install 1147396723 # Whatsapp
 # mas install 595191960 # CopyClip
-mas install 409201541 # Pages
-mas install 409183694 # Keynote
-mas install 409203825 # Numbers
-mas install 806101067 # QVR Client
-mas install 497799835 # xCode
+#mas install 409201541 # Pages
+#mas install 409183694 # Keynote
+#mas install 409203825 # Numbers
+#mas install 806101067 # QVR Client
+#mas install 497799835 # xCode
 
-brew install --cask tower
-brew install --cask babeledit
+#brew install --cask tower
+#brew install --cask babeledit
 brew install --cask visual-studio-code
-brew install --cask docker
+#brew install --cask docker
 brew install --cask dropbox
 brew install --cask google-chrome
 brew install --cask iterm2
 brew install --cask postman
-brew install --cask anydesk
-brew install --cask whisky
-brew install --cask tunnelblick
+#brew install --cask anydesk
+#brew install --cask whisky
+#brew install --cask tunnelblick
 # brew install --cask termhere
-brew install --cask little-snitch
-brew install --cask imageoptim
+#brew install --cask little-snitch
+#brew install --cask imageoptim
 # brew install --cask virtualbox
-brew install --cask adobe-creative-cloud
-brew install --cask zoom
+#brew install --cask adobe-creative-cloud
+#brew install --cask zoom
 # brew install --cask mongodb-compass
 # brew install --cask signal
-brew install --cask parallels
-brew install --cask omnidisksweeper
-brew install --cask notion
-brew install --cask asana
-brew install --cask battle-net
-brew install --cask nordvpn
+#brew install --cask parallels
+#brew install --cask omnidisksweeper
+#brew install --cask notion
+#brew install --cask asana
+#brew install --cask battle-net
+#brew install --cask nordvpn
